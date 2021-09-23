d89429332ba2f57568b482eb4b744a9035ce4ba7
Joan Wilson <joanwmoliveira@gmail.com>
2021-09-23 10:00:43 -0300

    Aprendendo Firebase

--- firebase/app.js
+++ firebase/app.js
@@ -1,21 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDaLjcfsjQ42BZUGeeFDuVJ46IOWkCirsI",
-  authDomain: "liva-756a0.firebaseapp.com",
-  projectId: "liva-756a0",
-  storageBucket: "liva-756a0.appspot.com",
-  messagingSenderId: "258151414059",
-  appId: "1:258151414059:web:92e6b3083f49cb3743ad6e",
-  measurementId: "G-RMBFXS6R2J"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+let data = firebase.firestore();
+  data.collection("pici").get()
+    .then((snapshot) => {
+      snapshot.foreach((doc) => {
+        console.log(doc)
+      })
+    })
